943add59819b550b841ed299e46e2c8d9235117d
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-25 15:20:24 +0300

    25/06/2024

---
+++ re_extracter/android/app/src/main/kotlin/com/example/re_extracter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.re_extracter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ re_extracter/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="RE extracter";
+  static const appName="re_extracter_flutter";
+  static const appDirName="re_extracter";
+  static const version=1.0;
+  static const description="type RE code and get result";
+}
